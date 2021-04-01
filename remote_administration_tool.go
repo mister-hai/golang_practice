b4f071db90a5fec1dfa7c1ed55159593838caaf0
mister-hai <81480735+mister-hai@users.noreply.github.com>
2021-04-01 19:39:41 -0400

    is this my house?

--- remote_administration_tool.go
+++ remote_administration_tool.go
@@ -16,78 +16,99 @@ package main
 
 // import the libraries we need
 import (
-	"color"
 	"bufio"
 	"crypto"
-	"crypto/sha256"
-	"crypto/sha512"
+	"fmt"
 	"io"
 	"io/ioutil"
-	"encoding/json"
-	"fmt"
+	"log"
 	"net"
-	"os"
 	"strings"
 )
 
-// declaring global variables to share our network information between scopes
+// declaring global variables to share our
+// network information between scopes
 // these are for TCP specifically
 var local_tcpaddr_LAN net.TCPAddr
 var local_tcpaddr_WAN net.TCPAddr
 
-//Admin Password in an obvious place
+//-----NAME-------------TYPE-----
+
+// Admin Password in an obvious place
 // TODO: set these for "hardmode" section
-var sha256_admin_pass_preencrypted sha256 = "1cbec737f863e4922cee63cc2ebbfaafcd1cff8b790d8cfd2e6a5d550b648afa"
-var sha512_admin_pass_preencrypted sha512;
+var sha256_admin_pass_preencrypted crypto.Hash
+var sha512_admin_pass_preencrypted crypto.Hash
 
 // Horribly insecure implementation
-var sha256_admin_pass_insecure sha256 = sha256.Sum256([]byte("admin"))
+var sha256_admin_pass_insecure crypto.Hash
+
+//sha256_admin_pass_insecure.Sum256([]byte("admin"))
 
 // struct to represent an OS command from the wire
 // we will be shoving a JSON payload into this
 type Command struct {
-	Task_id int
+	Task_id         int
 	command_string  string
 	info_message    string
 	success_message string
 	failure_message string
 }
-/* 
+
+/*
 function to hash a string to compare against the hardcoded password
  never hardcode a password in plaintext
  we use the strongest we can and a good password...
 
- For the putposes of this tutorial, we use a weak password.
+ For the porpoises of this tutorial, we use a weak password.
 */
 func hash_auth_check(password string) {
 	//Various Hashes, in order of increasing security
 	// dont use this
-	md5_password_hash := md5.Sum([]byte(password))
+	md5_password_hash := crypto.MD5.New()
+	md5_password_hash.Sum([]byte(password))
 	// or this
-	sha1_password_hash := sha1.Sum([]byte(password))
+	sha1_password_hash := crypto.MD5SHA1.New()
+	sha1_password_hash.Sum([]byte(password))
 	// this is ok-ish, if you have a long password
-	sha256_password_hash := sha256.Sum256([]byte(password))
+	sha256_password_hash := crypto.SHA512_256.New()
+	sha256_password_hash.Sum([]byte(password))
 }
+
 // the obvious, a plaintext password, hardcoded
-func insecure_password_check(password string){
+func insecure_password_check(password string) {
 
 }
+
 // function to get the hash of a file for integrity checking
-func file_hash(path string){
+func file_hash(path string) {
 	// read the file from path
 	file_input, error := ioutil.ReadFile(path)
-	// create hash instance
-	file_hash := sha256.New()
-	// compute hash , if error, log error
-	if _, err := io.Copy(file_hash, input); err != nil {
-    	log.Fatal(err)
+	//generic error printing
+	// if error isnt empty/null/nothingness
+	if error != nil {
+		// print the error
+		fmt.Println(error)
+		return
 	}
+	file_bytes, error := io.Reader.Read(file_input)
+	// create hash instance
+	file_hash := crypto.SHA256.New()
+	// copy file buffer to hash compute buffer
+	// if error, log error
+	// the underscore character "_" is called a "blank identifier"
+	// it allows you to ignore left side values
+	// in this case, we are acting like the regular return value doesnt exist
+	// and if there is an error, log that error and exit
+	if _, error := io.Copy(file_hash, file_input); error != nil {
+		log.Fatal(error)
+	}
+
 	sum := hash.Sum(nil)
 }
+
 // function to provide outbound connections via threading
-func tcp_outbound(laddr net.TCPAddr,
-	raddr net.TCPAddr,
-	port int8) {
+//-----------------Local IP---------Remote IP---------PORT-------
+func tcp_outbound(laddr net.TCPAddr, raddr net.TCPAddr, port int8) {
 	// the network functions return two objects
 	// a connection
 	// and an error
@@ -103,7 +124,7 @@ func tcp_outbound(laddr net.TCPAddr,
 	// will be basis of control flow
 	for {
 		netData, error := bufio.NewReader(connection).ReadString('\n')
-		
+
 		if error != nil {
 			fmt.Println(error)
 			return
@@ -115,7 +136,6 @@ func tcp_outbound(laddr net.TCPAddr,
 			return
 		}
 		connection.Write([]byte(myTime))
-	}
 	}
 }
 
